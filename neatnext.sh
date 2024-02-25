841ef1bfcf05644bcd8bffe56854ec218c072a6f
Nico Hämäläinen <nibsivaan@gmail.com>
2024-02-25 10:24:45 +0200

    add tailwind-forms to config
    Signed-off-by: Nico Hämäläinen <nibsivaan@gmail.com>

--- neatnext.sh
+++ neatnext.sh
@@ -197,7 +197,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {},
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 export default config
 EOF
